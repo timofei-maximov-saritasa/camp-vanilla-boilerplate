bfad6e5851ba355d4d65d18a0142da6a0ece9618
Timofei Maximov <timofei.maximov@saritasa.com>
2022-01-24 19:22:45 +0700

    inital commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root,
+  plugins: [],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'example', 'index.html'),
+      }
+    }
+  }
+})
